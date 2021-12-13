3dd7189af73c00af37f22c920b59f8a4962e990f
Alvarez Lopez, Javier Alejandro <javier.alvarez@astrazeneca.com>
2021-12-13 08:11:12 -0600

    PyLint the code, add module requirements

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-from ytmusicapi import YTMusic as yt
+"""
+This module aims to ease the process of managing the songs in a
+YouTube Music account. See docstrings for further information
+"""
 from icecream import ic
-import json
+from ytmusicapi import YTMusic as yt
 
-session = yt("headers_auth.json")
+SESSION = yt("headers_auth.json")
 
 
 def test():
-    songList = session.get_library_songs()
-    ic("This is the first song {}".format(songList[0]))
+    """This is a test function to probe authentication"""
+    song_list = SESSION.get_library_songs()
+    ic(f"This is the first song {song_list[0]}")
 
 def retrieve_library_songs(limit=5000):
+    """Function queries all library songs an returns a list with all of them"""
     cleanlist = []
-    songlist = session.get_library_songs(limit)
+    songlist = SESSION.get_library_songs(limit)
     for song in songlist:
         try:
             cleanlist.append({'title': song['title'],
                               'artist': song['artists'][0]['name'],
                               'album': song['album']['name'],
                               'ID': song['videoId']})
-        except:
-            ic("Error in {}".format(song['title']))
+        except TypeError as err:
+            ic(f"Error in {song['title']}")
+            ic(song, err)
     return cleanlist
 
 def retrieve_uploaded_songs(limit=5000):
-    cleanlist = []
-    songlist = session.get_library_upload_songs(limit)
-    for song in songlist:
+    """Function queries all uploaded songs an returns a list with all of them"""
+    clean_list = []
+    song_list = SESSION.get_library_upload_songs(limit)
+    for song in song_list:
         try:
             title = ""
             artist = ""
             album = ""
-            id_num = "" 
+            id_num = ""
             if 'title' in song:
                 title = song['title']
             if 'artists' in song:
@@ -41,66 +48,60 @@ def retrieve_uploaded_songs(limit=5000):
             if 'videoId' in song:
                 id_num = song['videoId']
 
-            songDict = {'title': title,
-                              'artist': artist,
-                              'album': album,
-                              'ID': id_num}
-            cleanlist.append(songDict)
+            song_dict = {'title': title,
+                        'artist': artist,
+                        'album': album,
+                        'ID': id_num}
+            clean_list.append(song_dict)
         except TypeError as err:
-            ic("{} error, not available".format(song['title']))
-            ic(song,err)
-
-    return cleanlist
+            ic(f"{song['title']} error, not available")
+            ic(song, err)
+    return clean_list
 
 def create_master_list():
-    playlistId = 0
-    songlist = []
-    playlists = session.get_library_playlists()
+    """Creates a playlist containing all songs from uploads and library"""
+    playlist_id = 0
+    song_list = []
+    playlists = SESSION.get_library_playlists()
     for playlist in playlists:
-        if(playlist['title'] == "Master List"):
-            ic("List found, ID {}".format(playlist['playlistId']))
-            playlistId = playlist['playlistId']
-    if(playlistId == 0):
+        if playlist['title'] == "Master List":
+            ic(f"List found, ID {playlist['playlistId']}")
+            playlist_id = playlist['playlistId']
+    if playlist_id == 0:
         ic("List not found, creating...")
-        playlistId = session.create_playlist("Master List", "List with all my songs")
-        ic("List created, ID {}".format(playlistId))
+        playlist_id = SESSION.create_playlist("Master List", "List with all my songs")
+        ic(f"List created, ID {playlist_id}")
     library = retrieve_library_songs(5000)
     uploads = retrieve_uploaded_songs(5000)
     masterlist = library + uploads
-    ic("{} elements in master list \n{} elements in library \n{} elements in uploads".format(len(masterlist),
-                                                                                                len(library),
-                                                                                                len(uploads)))
+    ic(f"{len(masterlist)} elements in master list\n" +
+       f"{len(library)} elements in library\n" +
+       f"{len(uploads)} elements in uploads")
     for song in masterlist:
-        songlist.append(song['ID'])
-    ic(session.add_playlist_items(playlistId, songlist, duplicates=False))
+        song_list.append(song['ID'])
+    ic(SESSION.add_playlist_items(playlist_id, song_list, duplicates=False))
 
 def export_list():
+    """Creates a csv file with all the songs from library and uploads"""
     all_songs = retrieve_library_songs() + retrieve_uploaded_songs()
     with open("song_list.csv", 'w', encoding='utf-8') as output:
         output.write("title;artist;album;ID\n")
         for song in all_songs:
-            line = song['title'] + ";" + song['artist'] + ";" + song['album'] + ";" + song['ID'] + "\n"
+            line = song['title'] + ";" + song['artist'] + ";" + \
+                   song['album'] + ";" + song['ID'] + "\n"
             output.write(line)
-
-def update_libary():
-    uploads = retrieve_uploaded_songs(10)
-    for song in uploads:
-        results = session.search("{} {}".format(song['title'], song['artist']), limit=2)
-        ic(results)
 
 if __name__ == "__main__":
     import sys
-    
     args = sys.argv[1:]
     ic(args)
-    
-    if(args[0] == "test"):
+    if args[0] == "test":
         test()
-    elif(args[0] == "setup"):
+    elif args[0] == "setup":
         yt.setup(filepath="headers_auth.json")
-    elif(args[0] == "export"):
+    elif args[0] == "export":
         export_list()
-    elif(args[0] == "masterlist"):
+    elif args[0] == "masterlist":
         create_master_list()
 
     ic("End")
